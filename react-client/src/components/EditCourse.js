e6011fe3b2ef4720acdbb8ad77908c89799f7b3b
Victoria <victorialiu38@gmail.com>
2022-03-06 17:26:04 -0500

    validation for edit courses and final build?

--- react-client/src/components/EditCourse.js
+++ react-client/src/components/EditCourse.js
@@ -8,40 +8,86 @@ import { withRouter } from 'react-router-dom';
 
 function EditCourse(props) {
   console.log('edituser props:',props.match.params)
-  const [course, setCourse] = useState({ _id: '', courseCode: '', courseName: '', 
-  section: '', semester: '' });  
+  //const [course, setCourse] = useState({ _id: '', courseCode: '', courseName: '', section: '', semester: '' });  
+  
+  const [courseCode, setCourseCode] = useState('');
+  const [courseName, setCourseName] = useState('');
+  const [section, setSection] = useState('');
+  const [semester, setSemester] = useState('');
   const [showLoading, setShowLoading] = useState(true);
+  
   const apiUrl = "http://localhost:3000/api/courses/" + props.match.params.id;
+  
+  let validatedSection = false;
+  let validatedInput = false;
   //runs only once after the first render
   useEffect(() => {
     setShowLoading(false);
     //call api
     const fetchData = async () => {
       const result = await axios(apiUrl);
-      setCourse(result.data);
+      //setCourse(result.data);
       console.log(result.data);
       setShowLoading(false);
+      setCourseCode(result.data.courseCode);
+      setCourseName(result.data.courseName);
+      setSection(result.data.section);
+      setSemester(result.data.semester);
     };
 
     fetchData();
   }, []);
 
-  const updateCourse = (e) => {
-    setShowLoading(true);
+  const handleInputSection = (e) => {
+    let value = e
+    const re = /^[0-9\b]+$/;
+    if (re.test(value)) {
+      setSection(value);
+      validatedSection = true;
+    }
+    else{
+      window.alert('Section: Please use numbers only.');
+      validatedSection = false;
+    }
+  }
+
+  const handleInputValidation = ()=> {
+    if(courseCode !== '' && courseName !== '' && section !== '' && semester !== '') {
+      handleInputSection(section);
+        if(validatedSection) {
+            validatedInput = true;
+        }
+    } else {
+        validatedInput = false;
+    }
+  }
+
+  const handleUpdateCourse = async (e) =>{ 
     e.preventDefault();
-    const data = { courseCode: course.courseCode, courseName: course.courseName, section: course.section, semester: course.semester};
-    axios.put(apiUrl, data)
-      .then((result) => {
-        console.log('after calling put to update',result.data )
+    validatedInput = false;
+    handleInputValidation();
+
+    if(validatedInput){
+      setShowLoading(true);
+      const data = {courseCode, courseName, section, semester};
+
+      axios.put(apiUrl, data).then((result) => {
+        window.alert(`Course updated successfully`)
+        setCourseCode('');
+        setCourseName('');
+        setSection('');
+        setSemester('');
         setShowLoading(false);
+        console.log('after calling put to update',result.data )
         props.history.push('/showcourse/' + result.data._id)
       }).catch((error) => setShowLoading(false));
-  };
-  //runs when user enters a field
-  const onChange = (e) => {
-    e.persist();
-    setCourse({...course, [e.target.name]: e.target.value});
-  }
+    } 
+    else {
+      window.alert('Please fill out all the fields!');
+    }
+};    
+
+  
 
   return (
     <div>
@@ -51,27 +97,31 @@ function EditCourse(props) {
         </Spinner> 
       } 
       <Jumbotron>
-        <Form onSubmit={updateCourse}>
-          <Form.Group>
-            <Form.Label> Course Code</Form.Label>
-            <Form.Control type="text" name="courseCode" id="courseCode" placeholder="Enter course code" value={course.courseCode} onChange={onChange} />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label> Course Name</Form.Label>
-            <Form.Control type="text" name="courseName" id="courseName" placeholder="Enter course name" value={course.courseName} onChange={onChange} />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label> Section</Form.Label>
-            <Form.Control type="text" name="section" id="secton" placeholder="Enter section" value={course.section} onChange={onChange} />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label> Semester</Form.Label>
-            <Form.Control type="text" name="semester" id="semester" placeholder="Enter semester" value={course.semester} onChange={onChange} />
-          </Form.Group>    
-          <Button variant="primary" type="submit">
-            Update Course
-          </Button>
-        </Form>
+        <Form onSubmit={handleUpdateCourse}>
+        <Form.Group>
+                  <Form.Label> Course Code</Form.Label>
+                  <Form.Control type="text" name="courseCode" id="courseCode" placeholder="Enter course code" 
+                                value={courseCode} onChange={e => setCourseCode(e.target.value)} />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label> Course Name</Form.Label>
+                  <Form.Control type="text" name="courseName" id="courseName" placeholder="Enter course name"
+                                value={courseName} onChange={e => setCourseName(e.target.value)} />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label> Section </Form.Label>
+                  <Form.Control type="text" name="section" id="section" placeholder="Enter section"
+                                value={section} onChange={e => setSection(e.target.value)} />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label> Semester </Form.Label>
+                  <Form.Control type="text" name="semester" id="semester" placeholder="Enter semester"
+                                value={semester} onChange={e => setSemester(e.target.value)} />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Update Course
+                </Button>
+              </Form>
       </Jumbotron>
     </div>
   );
