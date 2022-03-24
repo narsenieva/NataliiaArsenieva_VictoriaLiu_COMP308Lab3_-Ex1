62d87939a34b99cdf1dc8ac2a4b9427370e7a7f5
Nataliia Arsenieva <nataly.arsenieva@gmail.com>
2022-03-24 00:40:49 -0400

    Lab03 - Initial Commit

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
 //
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
+//
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+    cache: new InMemoryCache()
+  });
+  
+  //
+  ReactDOM.render(
+  
+    <React.StrictMode>
+  
+      <ApolloProvider client={client}>
+  
+      <App/>
+  
+      </ApolloProvider>
+  
+    </React.StrictMode>,
+  
+    document.getElementById('root')
+  );
--- server.js
+++ server.js
@@ -8,13 +8,26 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 // Load the module dependencies
 var mongoose = require('./config/mongoose'),
     express = require('./config/express');
+
+const { graphqlHTTP } = require('express-graphql');
+var schema = require('./app/graphql/studentSchemas');
+var cors = require("cors");
 // Create a new Mongoose connection instance
 var db = mongoose();
 // Create a new Express application instance
 var app = express();
+
+//configure GraphQL to use over HTTP
+app.use('*', cors());
+app.use('/graphql', cors(), graphqlHTTP({
+  schema: schema,
+  rootValue: global,
+  graphiql: true,
+}));
 // Use the Express application instance to listen to the '3000' port
-app.listen(5000);
+// Use the Express application instance to listen to the '4000' port
+app.listen(4000, () => console.log('Express GraphQL Server Now Running On http://localhost:4000/graphql'));
 // Use the module.exports property to expose our Express application instance for external usage
 module.exports = app; //returns the application object
 // Log the server status to the console
-console.log('Server running at http://localhost:5000/');
+//console.log('Server running at http://localhost:5000/');
